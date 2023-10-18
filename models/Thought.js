80a98b1ea1357bd6dc364c5005b07f8b9ce32678
Linh Lien <lienx149@gmail.com>
2023-10-18 13:40:44 -0500

    finished thought schema and reaction schema

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,41 @@
-const { Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
 
-const thoughtSchema = new Schema ({
+const reactionSchema = new Schema({
+
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+
+    reactionBody: {
+        type: String,
+        require: true,
+        maxLength: 280,
+    },
+
+    username: {
+        type: String,
+        require: true,
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    }
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+
+);
+
+const thoughtSchema = new Schema({
 
     thoughtText: {
         text: String,
@@ -10,12 +45,29 @@ const thoughtSchema = new Schema ({
     },
 
     createdAt: {
+        type: Date,
         default: Date.now,
-
-    }, 
+        get: createdAtVal => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+    },
 
     username: {
-        type: String, 
-        
-    }
-})
+        type: String,
+        require: true,
+    },
+    reactions: [ReactionSchema]
+},
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    });
+
+    thoughtSchema.virtual("reactionCount").get(function(){
+        return this.reactions.length;
+    })
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
